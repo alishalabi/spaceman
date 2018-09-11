dda1f636f33664c71bf17b724f1b79a89193874d
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-09-11 11:08:57 -0700

    Functionality added, only thing missing is drawing of spaceman

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # spaceman
+
+Requirements
+If the guessed letter is in the mystery word, the position(s) of the letter(s) are revealed in the placeholders.
+If a guessed letter occurs more than once in the word all the places that letter occurs are revealed.
+If a player guesses all the letters in the word, they win the game.
+For each incorrect guess a part of the Spaceman (a 7 part drawing of a Spaceman) is drawn.
+If all 7 parts of the Spaceman are drawn then the player loses the game.
+
+Pseudo-code:
+- Get word from Player 1 input
+- Hide word behind behind underscores and print word
+- Get guess from Player 2
+- Store guess in array of guesses
+- Run through each letter of word, if the guess matches a letter in the word then reveal that word (change the hidden word array, as well). If the word does not contain that guess, subtract one life
+- If hidden word contains no underscores, players wins game
+- If lives == 0, game over
+- If lives > 0 and game is not over, play another turn
--- spaceman.py
+++ spaceman.py
@@ -1,151 +1,76 @@
-# Import miscellaneous operating system interfaces (os) - This will help format temrinal
-import os
-
-# Set introduction
-
 # Set initial greating and input request from Player 1
 word = input("Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: ")
 
 # Set intial arrays: the word that will be shown, the guesses, and the number of chances remaining
-shownWord = ""
-guesses = []
-lives = 5
+shownWord = []
+pastGuesses = []
+currentGuess = ""
+lives = 7
 
-# Instructions for Player 2
-
+# Hide the word from Player 2
+def hide_word():
+    global shownWord
+    for char in word:
+        shownWord.append("_")
 
 # Request user input from Player 2
 def get_character():
-    global guesses
+    global pastGuesses
+    global currentGuess
     print("Enter your guess.")
     guess = input(">>>")
-    guesses.append(guess)
+    currentGuess = guess
+    pastGuesses.append(guess)
 
 
-# Add code to show blank guesses and correct guesses
-def showWord():
-    global shownWord
+
+# Run through each letter in word. Adding method to remove lives in this function
+# to take advantage of
+# Working on!
+def check_and_show_guess():
     global lives
-    for char in word:
-        if char not in guesses:
-            shownWord = shownWord + str("_")
-            lives -= 1
-        else:
-            shownWord = shownWord + str(char)
-        #
-        # if char in guesses:
-        #     shownWord = shownWord + str(char)
-        # else:
-        #     shownWord = shownWord + str("_")
-        #     lives -= 1
-    print(shownWord)
+    correctGuess = False
+    for i, value in enumerate(word):
+        if word[i] == currentGuess:
+            shownWord[i] = currentGuess
+            correctGuess = True
+    if correctGuess == False:
+        lives -= 1
+    # return shownWord
 
-# def test():
-#     showWord()
-#     print(lives)
-#
-# test()
 
-# # Enter running code
+# Function with all the methods to play game a turn
+def play_turn():
+    print(''.join(pastGuesses))
+    print("Lives remaining: " + str(lives))
+    print(''.join(shownWord))
+    get_character()
+    check_and_show_guess()
+    # Compare guess with word
+    # Print "correct"/"incorrect"
+    # Print hangman
+
+# Running code - play until win or lose
+hide_word()
 running = True
 while running:
-    if word == ''.join(shownWord):
-        print("You have guessed the correct word! Player 2 wins. Game Over.")
+    if "_" not in shownWord:
         running = False
+        print ("Game over, you win. You are amazing!")
     elif lives == 0:
-        print("Game over! Player 2 loses. Game Over")
         running = False
+        print("Game over, you lose.")
     else:
-        get_character()
-        showWord()
+        play_turn()
 
 
 
-
-# def get_character():
-#     global lives
-#     while lives > 0:
-#         input_item = user_input("Enter Guess:")
-#         add_guess(input_item)
-#         showWord()
-#         print(''.join(guesses))
-
-
-# def test():
-#     get_character()
-#
-#
-# test()
-
-
-
-# Second attempt:
-# # Set initial greating and input request from Player 1
-# word = input("Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: ")
-#
-# # Set intial arrays: the word that will be shown, the guesses, and the number of chances remaining
-# shownWord = []
-# pastGuesses = ["e","p","x"]
-# currentGuess = "e"
-# lives = 5
-#
-# # Hide the word from Player 2
-# def hide_word():
-#     global shownWord
-#     for char in word:
-#         shownWord.append("_")
-#
-# # Request user input from Player 2
-# def get_character():
-#     global pastGuesses
-#     global currentGuess
-#     print("Enter your guess.")
-#     guess = input(">>>")
-#     currentGuess = guess
-#     pastGuesses.append(guess)
-#
-#
-#
-# # Run through each letter in word. If guess
-# # Working on!
-# def check_and_show_guess():
-#     for i, value in enumerate(word):
-#         if word[i] == currentGuess:
-#             shownWord[i] = currentGuess
-#     return shownWord
-#
-#
-#
-#     # index = 0
-#     # global shownWord
-#     # for letter in word:
-#     #     if char in word:
-#     #         shownWord[index] = char
-#     #     else:
-#     #         return shownWord
-#
-#
-#
-#
-# # Instructions for Player 2
-#
-#
-#
-# # Function with all the methods to play game a turn
-# def play_turn():
-#     print(''.join(pastGuesses))
-#     print("Lives remaining: " + str(lives))
-#     print(''.join(shownWord))
-#     get_character()
-#     check_and_show_guess()
-#     # Compare guess with word
-#     # Print "correct"/"incorrect"
-#     # Print hangman
-#
-# # Test function
-# def test():
-#     hide_word()
-#     play_turn()
-#     print(shownWord)
-#
-# test()
+# Test function
+def test():
+    hide_word()
+    play_turn()
+    # print(''.join(shownWord))
+    # print(lives)
+    # play_turn()
+    # print(''.join(shownWord))
+    # print(lives)
