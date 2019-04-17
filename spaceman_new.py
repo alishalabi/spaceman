c16eb17829b2793058e99f2ec2b7d734a4eb1377
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-04-17 11:15:27 -0700

    Started refactoring new file

--- spaceman_new.py
+++ spaceman_new.py
@@ -1,16 +1,31 @@
 import os
 # Set initial greating and input request from Player 1
 os.system("clear")
-word = str.upper(input(
-    "Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: "))
+
+
+class Game:
+    def __init__(self, word):
+        """Initialize this node with the given data."""
+        self.word = str.upper(input(
+            "Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: ")]
+        self.lives = 7
+        self.shownWord = []
+        self.pastGuesses = []
+
+    def hide_word():
+        global shownWord
+        for char in word:
+            self.shownWord.append("_")
+
 
 # Set intial arrays: the word that will be shown, the guesses, and the number of chances remaining
+shownWord = []
+pastGuesses = []
+currentGuess = ""
+lives = 7
 
 # Hide the word from Player 2
 
-
-def hide_word():
-    pass
 
 # Request user input from Player 2
 # Adding function to check if guess is valid here
@@ -18,19 +33,34 @@ def hide_word():
 
 
 def get_character():
-    pass
+    global pastGuesses
+    global currentGuess
+    waitingForValidGuess = True
+    while waitingForValidGuess == True:
+        print("Guess a valid letter.")
+        guess = str.upper(input(">>>"))
+        if guess.isalpha() and len(guess) == 1 and guess not in pastGuesses:
+            currentGuess = guess
+            pastGuesses.append(guess)
+            waitingForValidGuess = False
 
 
 # Run through each letter in word. Adding method to remove lives in this function
 # to take advantage of
 # Working on!
 def check_and_show_guess():
-    pass
+    global lives
+    correctGuess = False
+    for index, value in enumerate(word):
+        if word[index] == currentGuess:
+            shownWord[index] = currentGuess
+            correctGuess = True
+    if correctGuess == False:
+        lives -= 1
     # return shownWord
 
+
 # Function with all the methods to play game a turn
-
-
 def play_turn():
     os.system('clear')
     # drawing.draw_spaceman(lives)
@@ -62,3 +92,8 @@ while running:
 def test():
     hide_word()
     play_turn()
+    # print(''.join(shownWord))
+    # print(lives)
+    # play_turn()
+    # print(''.join(shownWord))
+    # print(lives)
