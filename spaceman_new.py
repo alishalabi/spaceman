577a5041e9a2900479ba10de25459d60b3402b63
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-05-10 17:58:06 -0700

    Refactored, created class

--- spaceman_new.py
+++ spaceman_new.py
@@ -3,26 +3,29 @@ import os
 os.system("clear")
 
 
-class Game:
-    def __init__(self, word):
+class Spaceman():
+    def __init__(self):
         """Initialize this node with the given data."""
         self.word = str.upper(input(
-            "Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: ")]
+            "Welcome to Spaceman: Hangman for the 21st century. \nTo begin, player 1 should please enter a word: "))
         self.lives = 7
         self.shownWord = []
         self.pastGuesses = []
+        self.currentGuess = ""
 
-    def hide_word():
-        global shownWord
-        for char in word:
+    def get_input(self):
+        pass
+
+    def hide_word(self):
+        for char in self.word:
             self.shownWord.append("_")
 
 
 # Set intial arrays: the word that will be shown, the guesses, and the number of chances remaining
-shownWord = []
-pastGuesses = []
-currentGuess = ""
-lives = 7
+# shownWord = []
+# pastGuesses = []
+# currentGuess = ""
+# lives = 7
 
 # Hide the word from Player 2
 
@@ -32,60 +35,59 @@ lives = 7
 # Adding function to make all guesses uppercase
 
 
-def get_character():
-    global pastGuesses
-    global currentGuess
-    waitingForValidGuess = True
-    while waitingForValidGuess == True:
-        print("Guess a valid letter.")
-        guess = str.upper(input(">>>"))
-        if guess.isalpha() and len(guess) == 1 and guess not in pastGuesses:
-            currentGuess = guess
-            pastGuesses.append(guess)
-            waitingForValidGuess = False
+    def get_character(self):
+        # global pastGuesses
+        # global currentGuess
+        waitingForValidGuess = True
+        while waitingForValidGuess == True:
+            print("Guess a valid letter.")
+            guess = str.upper(input(">>>"))
+            if guess.isalpha() and len(guess) == 1 and guess not in self.pastGuesses:
+                self.currentGuess = guess
+                self.pastGuesses.append(guess)
+                waitingForValidGuess = False
+
+    # Run through each letter in word. Adding method to remove lives in this function
+    # to take advantage of
+    # Working on!
+    def check_and_show_guess(self):
+        correctGuess = False
+        for index, value in enumerate(self.word):
+            if self.word[index] == self.currentGuess:
+                self.shownWord[index] = self.currentGuess
+                correctGuess = True
+        if correctGuess == False:
+            self.lives -= 1
+        # return shownWord
+
+    # Function with all the methods to play game a turn
+
+    def play_turn(self):
+        os.system('clear')
+        # drawing.draw_spaceman(lives)
+        print("Past guesses: " + ', '.join(self.pastGuesses))
+        print("Lives remaining: " + str(self.lives))
+        print(' '.join(self.shownWord))
+        self.get_character()
+        self.check_and_show_guess()
+        # Compare guess with word
+        # Print "correct"/"incorrect"
+        # Print hangman
 
 
-# Run through each letter in word. Adding method to remove lives in this function
-# to take advantage of
-# Working on!
-def check_and_show_guess():
-    global lives
-    correctGuess = False
-    for index, value in enumerate(word):
-        if word[index] == currentGuess:
-            shownWord[index] = currentGuess
-            correctGuess = True
-    if correctGuess == False:
-        lives -= 1
-    # return shownWord
-
-
-# Function with all the methods to play game a turn
-def play_turn():
-    os.system('clear')
-    # drawing.draw_spaceman(lives)
-    print("Past guesses: " + ', '.join(pastGuesses))
-    print("Lives remaining: " + str(lives))
-    print(' '.join(shownWord))
-    get_character()
-    check_and_show_guess()
-    # Compare guess with word
-    # Print "correct"/"incorrect"
-    # Print hangman
-
-
+game = Spaceman()
 # Running code - play until win or lose
-hide_word()
-running = True
-while running:
-    if "_" not in shownWord:
-        running = False
+game.hide_word()
+game.running = True
+while game.running:
+    if "_" not in game.shownWord:
+        game.running = False
         print ("Game over, you win. You are amazing!")
-    elif lives == 0:
-        running = False
+    elif game.lives == 0:
+        game.running = False
         print("Game over, you lose.")
     else:
-        play_turn()
+        game.play_turn()
 
 
 # Test function
